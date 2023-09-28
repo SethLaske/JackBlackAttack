e497d465f05a96988fb2826f7cd4676949c3d293
Ricsdf <justincaseyrickyrobertlake@gmail.com>
2023-09-27 18:40:24 -0700

    Update RicWeapon.cs

--- Jack Black Attack/Assets/Weapons/RicWeapon.cs
+++ Jack Black Attack/Assets/Weapons/RicWeapon.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class RicWeapon : Weapon
 {
-    //Edit the base and charged attack so they can damage the enemy in the room.
-    //There are plenty of ways to do this, but the trick is to get the enemies BaseEnemy or Entity component and then use .TakeDamage(float damage) on it
+    public Collider2D attackBox;
+    public float damage = 10.0f;
+    public float attackDuration = 0.2f;
 
     protected override void BaseAttackOne()
     {
-        Debug.Log("Regular Attack");
+        StartCoroutine(PerformAttack());
+        Debug.Log("Base Attack");
     }
 
     protected override void ChargedAttackOne()
     {
-        Debug.Log("Charged Attack");
+        StartCoroutine(PerformAttack());
+        Debug.Lob("Charge Attack");
+    }
+
+    private IEnumerator PerformAttack()
+    {
+        attackBox.enabled = true;
+        yield return new WaitForSeconds(attackDuration);
+        attackBox.enabled = false;
+
+        Debug.Log("Attack Finished");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject other = collider.gameObject;
+        Entity enemyEntity = collider.GetComponent<Entity>();
+        if (enemyEntity != null && attackBox.enabled)
+        {
+            enemyEntity.TakeDamage(damage);
+        }
     }
 }
