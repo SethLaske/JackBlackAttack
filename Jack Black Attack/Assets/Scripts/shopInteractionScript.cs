79f0fb6d28ff0a4d1e2b54427f26d1f87231c6e1
EricShinozaki <emshinozaki@cpp.edu>
2023-11-13 14:17:03 -0800

    Start adding functionality to shop

--- Jack Black Attack/Assets/Scripts/shopInteractionScript.cs
+++ Jack Black Attack/Assets/Scripts/shopInteractionScript.cs
@@ -7,9 +7,42 @@ using TMPro;
 public class shopInteractionScript : MonoBehaviour
 {
     public Canvas myCanvas;
+
+    //Buttons
+    public Button boomerang;
+    public Button poolStick;
+    public Button trident;
+
+    //Prices
+
+    public int boomerangPrice;
+    public int poolStickPrice;
+    public int tridentPrice;
+
+    //Price display
+
+    public TextMeshProUGUI displayBoomerangPrice;
+    public TextMeshProUGUI displayPoolStickPrice;
+    public TextMeshProUGUI displaytridentPrice;
+
+    //Current player gold 
+    private int goldValue;
+    public TextMeshProUGUI playerGold;
+
+    private int bankedGold;
+    public TextMeshProUGUI bankValue;
+
+    //Display owned weapons
+
+    public TextMeshProUGUI ownBoomerang;
+    public TextMeshProUGUI ownPoolStick;
+    public TextMeshProUGUI ownTrident;
     void Start()
     {
-
+        goldValue = PlayerPrefs.GetInt("Player Gold");
+        playerGold.SetText("Player Gold: " + goldValue);
+        bankedGold = PlayerPrefs.GetInt("stored gold");
+        bankValue.SetText("Banked Gold: " + bankedGold);
     }
 
 
@@ -21,5 +54,20 @@ public class shopInteractionScript : MonoBehaviour
     public void exit()
     {
         myCanvas.gameObject.SetActive(false);
+    }
+
+    public void purchaseBoomerang()
+    {
+
+    }
+
+    public void purchasePoolStick()
+    {
+
+    }
+
+    public void purchaseTrident()
+    {
+
     }
 }
