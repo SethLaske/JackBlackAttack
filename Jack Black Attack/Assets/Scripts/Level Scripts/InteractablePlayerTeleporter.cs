5709135672057f968a78bd2e55cd7e4d598833f2
Ratske99 <sethlaske9999@gmail.com>
2023-11-12 17:54:47 -0800

    Fixed Dealer bug
    Couple other small fixes

--- Jack Black Attack/Assets/Scripts/Level Scripts/InteractablePlayerTeleporter.cs
+++ Jack Black Attack/Assets/Scripts/Level Scripts/InteractablePlayerTeleporter.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class InteractablePlayerTeleporter : MonoBehaviour
 {
     [SerializeField] private Transform newLocation;
-    private void OnTriggerStay2D(Collider2D collision)
+    private GameObject player;
+
+    private void Start()
+    {
+        player = null;
+    }
+    /*private void OnTriggerStay2D(Collider2D collision)
     {
         
         if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
@@ -13,6 +19,34 @@ public class InteractablePlayerTeleporter : MonoBehaviour
 
             collision.gameObject.transform.position = newLocation.position;
             BlackjackManager.Instance.NewHand();
+        }
+    }*/
+
+    private void Update()
+    {
+        if (player != null && Input.GetKeyDown(KeyCode.E))
+        {
+
+            player.transform.position = newLocation.position;
+            BlackjackManager.Instance.NewHand();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            player = null;
         }
     }
 }
