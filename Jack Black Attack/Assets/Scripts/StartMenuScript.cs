4e2106dbc348967743b8a3c18fcf8ca91be0c4cd
Ratske99 <sethlaske9999@gmail.com>
2023-11-20 23:26:33 -0800

    Updated start screen

--- Jack Black Attack/Assets/Scripts/StartMenuScript.cs
+++ Jack Black Attack/Assets/Scripts/StartMenuScript.cs
@@ -8,29 +8,41 @@ public class StartMenuScript : MonoBehaviour
 {
     public GameObject Controls;
     public GameObject MainMenu;
+    public GameObject player;
     //public Scene Hub;
+
+    private void Awake()
+    {
+        backToMenu();
+        ResetPrefabs();
+
+    }
+
     public void openHub()
     {
+        SceneManager.LoadScene("Hub");
+    }
+
+    private void ResetPrefabs() {
         PlayerPrefs.SetInt("Player Gold", 50);
         PlayerPrefs.SetString("ChosenWeapon", "PoolStick");
         PlayerPrefs.SetInt("stored gold", 50);
 
-        //Reset all other player prefs here
         PlayerPrefs.SetInt("boomerang", 0);
         PlayerPrefs.SetInt("pool stick", 1);
         PlayerPrefs.SetInt("trident", 0);
-
-
-        SceneManager.LoadScene("Hub");
     }
+
     public void displayControls()
     {
         MainMenu.SetActive(false);
         Controls.SetActive(true);
+        player.SetActive(true);
     }
     public void backToMenu()
     {
         MainMenu.SetActive(true);
         Controls.SetActive(false);
+        player.SetActive(false);
     }
 }
