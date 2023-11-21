c44f897293587e1284a7b4582bae78aafb0e8f82
Ratske99 <sethlaske9999@gmail.com>
2023-11-20 23:35:44 -0800

    Adjusted the ropes to seperate the victory area

--- Jack Black Attack/Assets/Scripts/Level Scripts/OneWayPath.cs
+++ Jack Black Attack/Assets/Scripts/Level Scripts/OneWayPath.cs
@@ -6,15 +6,18 @@ public class OneWayPath : MonoBehaviour
 {
     [SerializeField] private Collider2D blockArea;
     [SerializeField] private Vector2 correctDirection;
+    [SerializeField] private GameObject ropes;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Vector3.Dot(transform.position - collision.transform.position, correctDirection) > 0)
         {
             blockArea.enabled = false;
+            ropes.SetActive(false);
         }
         else {
             blockArea.enabled = true;
+            ropes.SetActive(true);
         }
     }
 }
