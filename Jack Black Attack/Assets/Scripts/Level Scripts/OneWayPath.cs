5e4def5270e516ffe3921939ee183c8a35320a73
Ratske99 <sethlaske9999@gmail.com>
2023-11-30 11:34:04 -0800

    Last second changes before build

--- Jack Black Attack/Assets/Scripts/Level Scripts/OneWayPath.cs
+++ Jack Black Attack/Assets/Scripts/Level Scripts/OneWayPath.cs
@@ -7,9 +7,18 @@ public class OneWayPath : MonoBehaviour
     [SerializeField] private Collider2D blockArea;
     [SerializeField] private Vector2 correctDirection;
     [SerializeField] private GameObject ropes;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (player != null && collision.gameObject != player) {
+            return;
+        }
         if (Vector3.Dot(transform.position - collision.transform.position, correctDirection) > 0)
         {
             blockArea.enabled = false;
